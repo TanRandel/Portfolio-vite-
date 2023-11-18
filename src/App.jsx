94e215f50c55621448fbb3da02f5ad6ca8e40619
Tan.Dev <136307769+TanRandel@users.noreply.github.com>
2023-11-18 14:31:45 +0800

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import Hero from './Hero'
 import './App.css'
-import { HashRouter as Router, Routes, Route } from 'react-router-dom'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 const App = () => {
   return (
     <Router>
